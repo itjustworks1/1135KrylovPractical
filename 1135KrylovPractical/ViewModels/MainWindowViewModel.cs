3542a4c9fa5444de2f93877f1561b377e7396dc4
user <1@mail.ru>
2026-02-02 15:29:20 +1000

    Ы

---
+++ 1135KrylovPractical/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,6 @@
+﻿namespace _1135KrylovPractical.ViewModels;
+
+public partial class MainWindowViewModel : ViewModelBase
+{
+    public string Greeting { get; } = "Welcome to Avalonia!";
+}
---
+++ 1135KrylovPractical/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace _1135KrylovPractical.ViewModels;
+
+public abstract class ViewModelBase : ObservableObject
+{
+}
